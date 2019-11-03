d236279aa6aeaa869316b9fb78fb3da37490e1a0
ben905713861 <905713861@qq.com>
2019-11-03 19:58:47 +0800

    option响应支持

--- blog-user/src/main/java/com/wuxb/blog/user/application/MyInterceptor.java
+++ blog-user/src/main/java/com/wuxb/blog/user/application/MyInterceptor.java
@@ -3,6 +3,7 @@ package com.wuxb.blog.user.application;
 import com.wuxb.httpServer.HttpServletRequest;
 import com.wuxb.httpServer.HttpServletResponse;
 import com.wuxb.httpServer.Interceptor;
+import com.wuxb.httpServer.ResponseHeader;
 import com.wuxb.httpServer.util.Config;
 
 public class MyInterceptor implements Interceptor {
@@ -11,7 +12,11 @@ public class MyInterceptor implements Interceptor {
 	
 	@Override
 	public void run(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)  throws Exception {
-		httpServletResponse.getHeader().set("Access-Control-Allow-Origin", USER_DOMAIN);
+		ResponseHeader header = httpServletResponse.getHeader();
+		header.set("Access-Control-Allow-Origin", USER_DOMAIN);
+		header.set("Access-Control-Allow-Methods", "POST,GET");
+		header.set("Access-Control-Allow-Headers", "Content-Type,random,time");
+		header.set("Access-Control-Max-Age", 3600);
 	}
 
 }
